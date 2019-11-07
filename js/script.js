d320e0424742d5cfc53f952692a748c4a6ec0690
englunddesigns <jonathanenglund@hotmail.co.uk>
2019-11-07 01:53:30 +0000

    Fixed Navbar Scrollbar issue

--- js/script.js
+++ js/script.js
@@ -4,8 +4,10 @@ $(document).ready(function(){
         var header = $(document).scrollTop();
         var headerHeight = $("nav").outerHeight();
         if (header > headerHeight) {
+          console.log("Made it Smaller");
           $("nav").addClass("py-0").removeClass("py-4");;
         } else {
+          console.log("Made it Bigger");
           $("nav").removeClass("py-0").addClass("py-4");
         }
     });
