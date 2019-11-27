56cc477d7c9c655d171a66125305dedc67662dc0
englunddesigns <jonathanenglund@hotmail.co.uk>
2019-11-27 17:13:37 +0000

    adjusted footer

--- js/script.js
+++ js/script.js
@@ -4,10 +4,8 @@ $(document).ready(function(){
         var header = $(document).scrollTop();
         var headerHeight = $("nav").outerHeight();
         if (header > headerHeight) {
-          console.log("Made it Smaller");
           $("nav").addClass("py-0").removeClass("py-4");;
         } else {
-          console.log("Made it Bigger");
           $("nav").removeClass("py-0").addClass("py-4");
         }
     });
